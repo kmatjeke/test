e53db02e0518a6478dc9d25be8b38b425b46a90e
kmatjeke <kmatjeke@student.wethinkcode.co.za>
2020-06-01 20:02:00 +0200

    first commit

--- srcs/lem_in.c
+++ srcs/lem_in.c
@@ -4,29 +4,29 @@
 int				main(void)
 {
 	int				*temp;
-	unsigned char	error_handler;
+	unsigned char	error_handler;		//variable will act as errorhandler for getting map information
 
-	error_handler = get_map_info();
+	error_handler = get_map_info();	//function which gets data from the map files.
 	if (error_handler == 1)
 	{
 		ft_putstr_fd("\nError\n", 2);
-		return (2);
+		return (-1);
 	}
-	temp = (int*)ft_memalloc(sizeof(int) * create_struct()->count);
-	temp[0] = create_struct()->ants;	
-	if ( ft_check_room(*room_fetch())|| create_struct()->ants <= 0)
+	temp = (int*)ft_memalloc(sizeof(int) * create_struct()->count);		//allocating the variable *temp memory.
+	temp[0] = create_struct()->ants;							//assigning the first index of integer array *temp, the value of the number of ants.
+	if ( ft_check_room(*room_fetch())|| create_struct()->ants <= 0) //if the number of ants are lower than 0, or if the is no start or end room.
 	{
-		ft_putstr_fd("\nError\n", 2);
-		return (2);
+		ft_putstr_fd("\nError\n", 2);					//print error.
+		return (-1);
 	}
-	if (solve_map(create_struct()->ants, -1, -1, 0))
+	if (solve_map(create_struct()->ants, -1, -1, 0))	//runs the function solve_map(which solves the map and removes unused paths)and if the function determines that the map is unsolvable.
 	{
-		ft_putstr_fd("\nError\n", 2);
-		return (2); 
+		ft_putstr_fd("\nError\n", 2);	//print error
+		return (-1); 
 	}
 	ft_putchar('\n');	
-	(void)print_results(create_struct()->ants, temp);	
-	free(temp);	
-	temp = NULL;	
+	(void)print_results(create_struct()->ants, temp);		//runs print_results which actually prints the solution of the map.
+	free(temp);					//frees memory to avoid memory leaks
+	temp = NULL;				//resseting the variable temp to null.
 	return (0);	
 }
